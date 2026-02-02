4293d64bc24a06d619b5467146d3ade51a1c8f92
Mark Veltzer <mark.veltzer@gmail.com>
2026-02-02 17:34:38 +0200

    no commit message given

--- scripts/change_many.sh
+++ scripts/change_many.sh
@@ -3,14 +3,14 @@
 for x in $(find src -name "*.c" -or -name "*.cc")
 do
 	# echo "doing [$x]"
-	if grep -v main $x | grep argv
+	if grep -v main "${x}" | grep argv
 	then
 		continue
 	fi
-	if grep -v main $x | grep argc
+	if grep -v main "${x}" | grep argc
 	then
 		continue
 	fi
-	echo $x
-	sed -i 's/^int main(int argc, char\*\* argv, char\*\* envp) {$/int main() {/' $x
+	echo "${x}"
+	sed -i 's/^int main(int argc, char\*\* argv, char\*\* envp) {$/int main() {/' "${x}"
 done
